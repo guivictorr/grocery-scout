2ac2b63acb1c4fc8a32561c0d53c30c044135e00
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-12-14 23:20:13 -0300

    feat: add mock data to the list

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,5 @@
 import { Stack } from "expo-router";
+
 import "@/ui/global.css";
 
 export default function RootLayout() {
--- src/ui/pages/home/home.tsx
+++ src/ui/pages/home/home.tsx
@@ -1,9 +1,37 @@
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
+
+function generateMockData(count: number) {
+  const items = ["Manteiga", "Queijo", "Leite", "Pão", "Ovos", "Iogurte"];
+
+  return Array.from({ length: count }, (_, index) => {
+    const randomItem = items[Math.floor(Math.random() * items.length)];
+    const randomDate = new Date(
+      Date.now() + Math.random() * 365 * 24 * 60 * 60 * 1000, // Data futura aleatória dentro de um ano
+    );
+
+    return {
+      id: `item-${index + 1}-${randomItem}`, // ID único com base no índice
+      name: randomItem,
+      expiration: randomDate,
+    };
+  });
+}
 
 export function Home() {
   return (
-    <View className="flex-1 items-center justify-center bg-red-300 dark:bg-green-100">
-      <Text className="text-red-50">Hello World</Text>
-    </View>
+    <FlatList
+      data={generateMockData(30)}
+      contentInsetAdjustmentBehavior="always"
+      keyExtractor={(item) => item.toString()}
+      ListEmptyComponent={() => <Text>Vazio</Text>}
+      renderItem={({ item }) => (
+        <View className="flex-row justify-between items-center px-4 py-3 border-b-[0.3px] border-gray-400">
+          <Text className="text-lg">{item.name}</Text>
+          <Text className="text-lg">
+            {item.expiration.toLocaleDateString("pt-br")}
+          </Text>
+        </View>
+      )}
+    />
   );
 }
