b598aeb290277700de2edf4ec681b0bd9d5bb368
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2025-01-19 02:28:01 -0300

    feat: add `setNotFoundHandler` and create `NotFoundError`

--- apps/api/src/infra/errors.ts
+++ apps/api/src/infra/errors.ts
@@ -1,3 +1,24 @@
+export class NotFoundError extends Error {
+  readonly statusCode: number;
+  readonly action: string;
+
+  constructor() {
+    super("This route doesn't exist.");
+    this.name = "NotFoundError";
+    this.statusCode = 404;
+    this.action =
+      "Verify if the address is correct or if the http verb is valid.";
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      status_code: this.statusCode,
+      action: this.action,
+    };
+  }
+}
 export class InternalServerError extends Error {
   readonly statusCode: number;
   readonly action: string;
