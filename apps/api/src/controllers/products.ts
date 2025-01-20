9a5cc1187d236361518926faece8b8bfba088d25
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2025-01-19 22:11:40 -0300

    feat: validate `request.body` of products controller

--- apps/api/src/controllers/products.ts
+++ apps/api/src/controllers/products.ts
@@ -1,8 +1,18 @@
-import product from "@/models/product";
 import { FastifyRequest, FastifyReply } from "fastify";
+import { z } from "zod";
+
+import product from "@/models/product";
+
+const productSchema = z.object({
+  name: z.string().min(3).max(50),
+  ean: z.string().min(8).max(13).regex(/^\d+$/, {
+    message: "The input must be a string of numbers (digits only).",
+  }),
+});
 
 async function post(request: FastifyRequest, reply: FastifyReply) {
-  const { name, ean } = request.body;
+  const { name, ean } = productSchema.parse(request.body);
+
   const productId = await product.create(name, ean);
   reply.status(201).send({ id: productId });
 }
--- apps/api/src/tests/products/post.test.ts
+++ apps/api/src/tests/products/post.test.ts
@@ -45,4 +45,44 @@ describe("POST /api/v1/products", () => {
         "Please verify the entity's unique identifier (e.g., EAN) and try again. If you intended to update the existing entity, consider using a PUT request instead.",
     });
   });
+  test("Creating a product with invalid ean", async () => {
+    const requestBody = {
+      name: "Valid Product",
+      ean: "invalid-ean",
+    };
+    const response = await fetch("http://localhost:3000/api/v1/products", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(requestBody),
+    });
+    const responseBody = await response.json();
+
+    expect(response.status).toBe(400);
+    expect(responseBody).toEqual({
+      status_code: 400,
+      name: "ValidationError",
+      message: "The property ean is invalid.",
+      action: "The input must be a string of numbers (digits only).",
+    });
+  });
+  test("Creating a product with invalid name", async () => {
+    const requestBody = {
+      name: "",
+      ean: "12345678",
+    };
+    const response = await fetch("http://localhost:3000/api/v1/products", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(requestBody),
+    });
+    const responseBody = await response.json();
+
+    expect(response.status).toBe(400);
+    expect(responseBody).toEqual({
+      status_code: 400,
+      name: "ValidationError",
+      message: "The property name is invalid.",
+      action: "String must contain at least 3 character(s)",
+    });
+  });
 });
