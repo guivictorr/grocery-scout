f623833ed02ab72c778242ecc78531082e59dddf
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2025-01-20 23:48:51 -0300

    feat: create `/api/v1/prices/:marketId` endpoint

--- apps/api/src/controllers/prices.ts
+++ apps/api/src/controllers/prices.ts
@@ -5,16 +5,24 @@ import { z } from "zod";
 const priceSchema = z.object({
   price_cents: z.number().positive(),
   productId: z.number().positive(),
-  marketId: z.number().positive(),
+  marketId: z.coerce.number().positive(),
 });
 async function post(request: FastifyRequest, reply: FastifyReply) {
   const { marketId, price_cents, productId } = priceSchema.parse(request.body);
   await price.create(price_cents, productId, marketId);
   reply.status(201).send({});
 }
+async function list(request: FastifyRequest, reply: FastifyReply) {
+  const { marketId } = priceSchema
+    .pick({ marketId: true })
+    .parse(request.params);
+  const prices = await price.list(marketId);
+  reply.status(200).send(prices);
+}
 
 const pricesController = {
   post,
+  list,
 };
 
 export default pricesController;
