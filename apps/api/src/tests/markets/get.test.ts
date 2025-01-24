e8f9cd1d7fda9435cdb7d05b8efa16b3c3a7f3c4
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2025-01-23 21:07:44 -0300

    test: use orchestrator functions instead of `fetch`

--- apps/api/src/tests/markets/get.test.ts
+++ apps/api/src/tests/markets/get.test.ts
@@ -7,15 +7,11 @@ beforeAll(async () => {
 
 describe("GET /api/v1/markets", () => {
   test("Retrieving markets", async () => {
-    await fetch("http://localhost:3000/api/v1/markets", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        name: "John Doe's Market",
-        lat: -28.3392173,
-        lon: -48.7040639,
-      }),
-    });
+    await orchestrator.createMarket(
+      "John Doe's Market",
+      -28.3392173,
+      -48.7040639,
+    );
 
     const response = await fetch("http://localhost:3000/api/v1/markets");
     const responseBody = await response.json();
