1b94be410900b0d59b2033eded0a41c675461408
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2025-01-23 21:59:24 -0300

    refactor: remove `zod` schema duplication

--- apps/api/src/controllers/products.ts
+++ apps/api/src/controllers/products.ts
@@ -12,20 +12,12 @@ const productSchema = z.object({
 
 async function post(request: FastifyRequest, reply: FastifyReply) {
   const { name, ean } = productSchema.parse(request.body);
-
   const productId = await product.create(name, ean);
   reply.status(201).send({ id: productId });
 }
-
-const getParamsSchema = z.object({
-  ean: z.coerce.string().min(8).max(13).regex(/^\d+$/, {
-    message: "The input must be a string of numbers (digits only).",
-  }),
-});
 async function get(request: FastifyRequest, reply: FastifyReply) {
-  const params = getParamsSchema.parse(request.params);
+  const params = productSchema.pick({ ean: true }).parse(request.params);
   const productResult = await product.findByEan(params.ean);
-
   reply.status(200).send(productResult.rows[0]);
 }
 
