bcf7f13c2b58d6bb2c1cf818aee1469ffbd35862
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2025-01-23 22:00:00 -0300

    refactor: change import order

--- apps/api/src/controllers/markets.ts
+++ apps/api/src/controllers/markets.ts
@@ -1,16 +1,16 @@
-import market from "@/models/market";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
+import market from "@/models/market";
 
 const marketSchema = z.object({
   name: z.string().min(3).max(50),
   lat: z.number(),
   lon: z.number(),
 });
+
 async function post(request: FastifyRequest, reply: FastifyReply) {
   const { name, lat, lon } = marketSchema.parse(request.body);
   const marketResult = await market.create(name, lat, lon);
-
   reply.status(201).send(marketResult);
 }
 async function get(request: FastifyRequest, reply: FastifyReply) {
